f1e48d201ec9f4e0cd4183de16cb5b58a17ef3e7
farzinnasiri <farzinnasiri890@gmail.com>
2020-04-05 20:06:59 +0430

    some changes

---
+++ src/ir/sharif/math/ap98/asteroid/Logic/AsteroidFactory.java
@@ -0,0 +1,32 @@
+package ir.sharif.math.ap98.asteroid.Logic;
+
+import ir.sharif.math.ap98.asteroid.Intefaces.ObjectFactory;
+import ir.sharif.math.ap98.asteroid.Models.Asteroid;
+import ir.sharif.math.ap98.asteroid.Util.GameConstants;
+import ir.sharif.math.ap98.asteroid.Util.Vector2D;
+//import main.java.Util.Constants;
+
+import java.util.Random;
+
+public class AsteroidFactory implements ObjectFactory<Asteroid> {
+    private Random random;
+    private int count;
+    private GameConstants constants;
+
+
+    public AsteroidFactory() {
+        random = new Random(System.nanoTime());
+        constants = GameConstants.getInstance();
+    }
+
+    @Override
+    public Asteroid createNew() {
+        count++;
+        System.out.println(count+" asteroids created");
+        int x = random.nextInt(constants.getConstant("maxWidth") - constants.getConstant("maxSize"));
+        int size = random.nextInt(constants.getConstant("maxSize") - constants.getConstant("minSize")) + constants.getConstant("minSize");
+        int y = -random.nextInt(constants.getConstant("maxHeight"));
+        int speedY = random.nextInt(constants.getConstant("maxSpeed") - constants.getConstant("minSpeed")) + constants.getConstant("minSpeed");
+        return new Asteroid(new Vector2D(x, y), size, 0, speedY, "asteroid");
+    }
+}
